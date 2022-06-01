a865a9fff0e4ea89d05635b381616616b4c81f26
RahulChauhan1008 <97465121+RahulChauhan1008@users.noreply.github.com>
2022-06-01 18:07:38 +0530

    Adding Firbase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ap_url: 'http://firebase.google.com',
+  api_key:'AIzaSyBkigeDJhv_MkmGz97IZionHmIsBan_FtI',
 };
 
 /*
